608e028d4ff4d9bb41335728e392d2a465e01f07
mitchell <husslet1@yahoo.com>
2022-10-29 06:11:19 +0100

    addfiles

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+
+import Nav from "./Nav"
+import Hero from "./Hero"
+import Footer from "./Footer"
+
+
+
+
+
+
+function Home() {
+
+  return (
+ 
+    <div className="App">
+       <Nav />
+       <Hero />
+       <Footer />
+
+       
+       
+      
+    </div>
+
+  
+   
+  
+    
+  );
+}
+
+export default Home;
